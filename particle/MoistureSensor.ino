a1aba4dba06a55a627604da3407efdccfe0e9957
David Swearingen <swearingendw@mac.com>
2015-11-20 19:45:08 -0500

    code clean up

--- particle/MoistureSensor.ino
+++ particle/MoistureSensor.ino
@@ -1,7 +1,17 @@
-// Read soil moisture sensor and photoresistor and write values to serial comms and particle, etc.
-// openweathermap.org bethesda city id = 4348599
-// http://api.openweathermap.org/data/2.5/forecast?id=<id>
+/* Read soil moisture sensor and photoresistor and write values to serial comms and particle, etc.
+ openweathermap.org bethesda city id = 4348599
+ http://api.openweathermap.org/data/2.5/forecast?id=<id>
+ To see events in the terminal: `particle subscribe davidws:moisture`
+ The moisture event is currently picked up by IFTTT.com and it sends it to a Google spreadsheet
+ Particle.publish("davidws:light", String(light), 240, PRIVATE);
+ To see Serial output in terminal:  `particle serial monitor /dev/tty.usbmodem1411`
+ First use `particle serial list` to see my online device address(es)
+*/
 
+float TOP_SCALE_NUMBER = 70.0;
+float BOTTOM_SCALE_NUMBER = 25.0;
+float TOP_SCALE_STEPPER_POSITION = 6700.0;
+float BOTTOM_SCALE_STEPPER_POSITION = 0.0;
 int UP = HIGH;
 int DOWN = LOW;
 int powerOut = A5;
@@ -20,8 +30,7 @@ int m;
 int buttonStatus = 0;
 int direction = A3;
 bool buttonChange = false;
-bool buttonTurnedOff = false;
-bool temp = true;
+bool buttonTurnedOff = false; // calibration momentary pushbutton was released
 void buttonChanged(void);
 
 void setup()
@@ -34,10 +43,8 @@ void setup()
     pinMode(readMoisture, INPUT);
     pinMode(step, OUTPUT);
     pinMode(direction, OUTPUT);
-    //digitalWrite(stepper, LOW);
 
-    // Just a test:
-    // Publish value to a rest endpoint, also get this via: particle variable get aardvark_crazy analogValue
+    // Just testing: Publish value to a rest endpoint, also get this via: particle variable get aardvark_crazy analogValue
     // or, HTTP GET /v1/devices/{DEVICE_ID}/{VARIABLE}
     Particle.variable("analogValue", analogValue);
 
@@ -45,25 +52,14 @@ void setup()
     // LED on the Particle
     Particle.function("blink", blinkMultiple);
     Particle.function("remoteMove", remoteMove);
-    // Particle.function("remotePos", remotePos);
-    // Particle.function("remoteStep", remoteStep);
-    // Particle.function("remoteTest", positionS);
+    Particle.function("remoteTemp", remoteTemp);
 
-    //Particle.function("", positionS);
+    // To monitor, e.g.: screen /dev/cu.usbmodem1411 9600
     Serial.begin(9600);   // open serial over USB
-    Serial.println("Hello Computer");
+    Serial.println("Getting started...");
 }
 
 void loop() {
-  if(temp) {
-    delay(5000);
-    moveToPosition(200);
-    delay(500);
-    moveToPosition(400);
-    delay(500);
-    moveToPosition(200);
-    temp = false;
-  }
     m = millis();
     if (m - last > (1000 * 60 * minutesDelay)) {
         last = m;
@@ -74,26 +70,17 @@ void loop() {
         delay(50);  // just in case the sensor needs a little time to power up
         analogValue = analogRead(readMoisture);
         digitalWrite(powerOut, LOW);  // turn off the moisture sensor when not taking a reading, to prevent oxidation
-        light = analogRead(readLight);
+        light = analogRead(readLight); // read the CdS light cell, just for fun
 
-        // To see events in the terminal: `particle subscribe davidws:moisture`
-        // The moisture event is currently picked up by IFTTT.com and it sends it to a Google spreadsheet
-        //Particle.publish("davidws:moisture", String(analogValue), 240, PRIVATE);
-        // Particle.publish("davidws:light", String(light), 240, PRIVATE);
-
-        // To see Serial output in terminal:  `particle serial monitor /dev/tty.usbmodem1411`
-        // First use `particle serial list` to see my online device address(es)
-        // Serial.println(analogValue);
     }
-
+    // This is the calibration button.  Hold it until reaching the calibration point, then released
+    // This resets the zero-location.
     if (buttonStatus == HIGH) {
       move(1, DOWN);
     }
 
     if (buttonTurnedOff) {
       position = 0;
-      Serial.println("calibration button is now off");
-      Serial.println(position);
       buttonTurnedOff = false;
     }
 }
@@ -132,10 +119,8 @@ void move(int n, int dir) {
     // Serial.println(position);
     Particle.publish("davidws:testing", String(position), 60, PRIVATE);
 }
-
+// Move stepper to a particular position
 void moveToPosition(int p) {
-    Serial.println("before moveToPosition:");
-    Serial.println(position);
     int dir = DOWN;
     int delta = p - position;
     int pol = abs(delta) / delta;
@@ -143,8 +128,12 @@ void moveToPosition(int p) {
       dir= UP;
     }
     move(abs(delta), dir);
-    Serial.println("after moveToPosition:");
-    Serial.println(position);
+}
+
+// Remotely tell indicator to move to a particular temperature
+int remoteTemp(String temperature) {
+  int x = ((temperature.toFloat() - BOTTOM_SCALE_NUMBER) / (TOP_SCALE_NUMBER - BOTTOM_SCALE_NUMBER)) * (TOP_SCALE_STEPPER_POSITION - BOTTOM_SCALE_STEPPER_POSITION);
+  moveToPosition((int) x);
 }
 
 int remoteMove(String i) {
@@ -157,56 +146,3 @@ void buttonChanged() {
     buttonTurnedOff = true;
   }
 }
-
-
-/*
-int remotePos(String s) {
-  Particle.publish("davidws:test", s, 60, PRIVATE);
-  positionS(s.toInt());
-}
-
-// tell stepper to go to an absolute position
-int positionS(int newPos) {
-    int delta = stepperPos - newPos;
-    int direction = delta / abs(delta);
-    step(delta, direction);
-    stepperPos = newPos;
-}
-
-// tell stepper to step n times in a direction, where direction is either a positive or negative number
-void step(int n, int direction) {
-    if (direction < 0) {
-        digitalWrite(stepperDirection, HIGH);
-    } else {
-        digitalWrite(stepperDirection, LOW);
-    }
-    for (int i = 0; i < n; i++) {
-        digitalWrite(stepper, HIGH);
-        delay(2);
-        digitalWrite(stepper, LOW);
-    }
-}
-
-
-// For testing, this is a registered Particle function so I can control the stepper remotely.
-// s is a String which will hold an int for number of steps.
-// Direction of stepping isn't important, at least not now.
-int remoteStep(String s) {
-    int dir = 1;
-    RGB.control(true);
-    RGB.color(255, 0, 0);
-    int c = s.toInt();
-    if (c < 0) {
-      dir = -1;
-    }
-    step(abs(c), dir);
-    RGB.control(false);
-}
-
-int remoteTest(String s) {
-    Particle.publish("davidws:test", s, 60, PRIVATE);
-}
-*/
-
-
-// Need a calibration routine to position stepper at the starting position of the gauge
